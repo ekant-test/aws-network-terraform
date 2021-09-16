fd9ccc4897668dfaa417f4e4ab54ab754e5bf07c
Ekant <ekant.mate@virginaustralia.com>
2021-09-16 13:35:55 +0530

    AWS Base Networking Template

--- modules/workloads_accounts/main.tf
+++ modules/workloads_accounts/main.tf
@@ -2,5 +2,5 @@ data "aws_caller_identity" "this" {}
 
 locals {
   account_id = data.aws_caller_identity.this.account_id
-  env  = "npr"
+  env        = "npr"
 }
--- modules/workloads_accounts/network.tf
+++ modules/workloads_accounts/network.tf
@@ -234,9 +234,9 @@ resource "aws_route_table_association" "fw" {
 }
 
 resource "aws_route" "fw" {
-  route_table_id            = aws_route_table.fw.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.fw.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 
