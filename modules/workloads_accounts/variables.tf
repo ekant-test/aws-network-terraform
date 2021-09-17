3f4434875dbc2d04683b59b55cf2496e5caf6df5
Ekant <41195678+ekant-test@users.noreply.github.com>
2021-09-17 08:05:47 +0530

    naming changes convention

--- modules/workloads_accounts/variables.tf
+++ modules/workloads_accounts/variables.tf
@@ -3,23 +3,6 @@ variable "common_tags" {
   description = "the common tags to apply to the managed resources"
 }
 
-
-# variable "accounts" {
-#   type = map(object({
-#     id          = string
-#     name        = string
-#     description = string
-#     arn         = string
-#     email       = string
-#     production  = bool
-#   }))
-#   description = "account information from landing zone required to setup the network"
-# }
-
 variable "default_vpc_cidr" {
   description = "CIDR block of the npr VPC"
 }
-
-# variable "cloud_id" {
-#   description = "the id of the DNS VPC hosting the shared Route53 resolver"
-# }
