2563729f29c1bb7bc6dfb523e2113cf2199af92f
Ekant <41195678+ekant-test@users.noreply.github.com>
2021-09-17 08:07:05 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,8 @@
 provider "aws" {
   alias = "wl_account"
-  # assume_role {
-  #   role_arn = "arn:aws:iam::${var.workload_account_id}:role/${var.cross_account_execution_role}"
-  # }
 }
 
+## this block is needed if you need to call the module for another account ##
 # provider "aws" {
 #   alias = "wl_account_1"
 #   assume_role {
